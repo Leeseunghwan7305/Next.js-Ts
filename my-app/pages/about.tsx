2e4c83c5668899afb98341f1ca8669ce4939a22e
seunghwan <sun1238799@naver.com>
2022-08-03 10:05:27 +0900

    getStaticProps

--- my-app/pages/about.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const about = () => {
-  return <div>여기의 경로는 /about입니다!</div>;
-};
-
-export default about;
--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -4,10 +4,37 @@ import Image from "next/image";
 import Link from "next/link";
 import { Router, useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
-
-const Home: NextPage = () => {
-  const router = useRouter();
-  return <div>여기는 시작페이지입니다!!</div>;
+import fs from "fs/promises"; //서버쪽 임폴트는 제거한다.
+// getStaticProps 도 제거 한다.
+import path from "path"; //경로 구축할떄 씀
+interface IProduct {
+  products: [
+    {
+      id: string;
+      title: string;
+    }
+  ];
+}
+const Home = (props: IProduct) => {
+  const { products } = props;
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
 };
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData: any = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products,
+    },
+  };
+}
 
 export default Home;
